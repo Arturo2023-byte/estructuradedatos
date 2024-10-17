9faa33a120f3b63de1d594e3d96ba3fbb4792b4a
david arturo ruiz chanchi <ruizdreidxyz2003@gmail.com>
2024-10-16 20:19:40 -0500

    primerComit

---
+++ VARIABLE.js
@@ -0,0 +1,17 @@
+//Primer paso para la creacion de una fomulacion algoritmica con el uso de variables
+
+//paso uno: Crear dos variables estables "osea sin cambio alguno"
+let numero1 = 10;
+let numero2 = 5;
+
+// Declaramos las siguientes operaciones matemáticas: Suma, resta y demas
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+// Mostramos los resultados
+console.log("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+console.log("La resta de " + numero1 + " y " + numero2 + " es: " + resta);
+console.log("La multiplicación de " + numero1 + " y " + numero2 + " es: " + multiplicacion);
+console.log("La división de " + numero1 + " y " + numero2 + " es: " + division);
